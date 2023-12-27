8bbfa70d08dd59790e0118974af3b8bfc41569e8
Kiran <kirankumarbs1034@gmail.com>
2023-12-27 20:56:32 +0530

    Add project files.

---
+++ JWT-Authentication-With-RefreshToken.SQL/dbo/Tables/RefreshTokens.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[RefreshTokens] (
+    [RefreshTokenId]  INT           IDENTITY (1, 1) NOT NULL,
+    [UserId]          INT           CONSTRAINT [DF_RefreshTokens_RefreshTokenId] DEFAULT ((-1)) NOT NULL,
+    [RefreshToken]    VARCHAR (200) NOT NULL,
+    [ExpireDateTime]  DATETIME      NOT NULL,
+    [CreatedByIp]     VARCHAR (50)  NOT NULL,
+    [RevokedDateTime] DATETIME      NULL,
+    [RevokedByIp]     VARCHAR (50)  NULL,
+    [RevokedUserId]   INT           NULL,
+    [ReplacedByToken] VARCHAR (200) NULL,
+    [RevokeReason]    VARCHAR (300) NULL,
+    [IsExpired]       CHAR (1)      NULL,
+    [IsActive]        CHAR (1)      CONSTRAINT [DF_RefreshTokens_IsActive] DEFAULT ('Y') NOT NULL,
+    [IsRevoked]       CHAR (1)      NULL,
+    [CreatedDateTime] DATETIME      CONSTRAINT [DF_RefreshTokens_CreatedDateTime] DEFAULT (getdate()) NOT NULL,
+    [CreatedUserId]   INT           NOT NULL,
+    [ModifiedUserId]  INT           NULL,
+    [Modifiedtime]    DATETIME      NULL,
+    CONSTRAINT [Pk_RefreshTokens_RefreshTokenId] PRIMARY KEY CLUSTERED ([RefreshTokenId] ASC),
+    CONSTRAINT [FK_RefreshTokens_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[UserDetails] ([UserId]) ON DELETE CASCADE,
+    CONSTRAINT [UNQ_RefreshTokens_RefreshToken] UNIQUE NONCLUSTERED ([RefreshToken] ASC)
+);
+
---
+++ JWT-Authentication-With-RefreshToken.SQL/dbo/Tables/UserDetails.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[UserDetails] (
+    [UserId]             INT                                               IDENTITY (1, 1) NOT NULL,
+    [FullName]           VARCHAR (20)                                      NOT NULL,
+    [LoginId]            VARCHAR (50)                                      NOT NULL,
+    [Address]            VARCHAR (200)                                     NOT NULL,
+    [EmailAddress]       VARCHAR (50)                                      NULL,
+    [IsActive]           BIT                                               NOT NULL,
+    [PasswordChangeDate] DATETIME                                          NULL,
+    [CreatedUserId]      INT                                               NOT NULL,
+    [CreatedDateTime]    DATETIME                                          NOT NULL,
+    [ModifiedUserId]     INT                                               NULL,
+    [ModifiedDate]       DATETIME                                          NULL,
+    [Password]           VARCHAR (20) MASKED WITH (FUNCTION = 'default()') NULL,
+    [PhoneNumber]        VARCHAR (10)                                      NULL,
+    CONSTRAINT [PK_UserDetails_UserId] PRIMARY KEY CLUSTERED ([UserId] ASC),
+    UNIQUE NONCLUSTERED ([LoginId] ASC)
+);
+
