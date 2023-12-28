6fb586f231e646e2db388d9eef8be797d9cc1605
Kiran <kirankumarbs1034@gmail.com>
2023-12-28 21:29:02 +0530

    Revert "Added Dependency Injection services in Service container."
    This reverts commit 388db10993601d57a7ac69153148c5b3793032e1.

--- JWT-Authentication-With-RefreshToken.API/Program.cs
+++ JWT-Authentication-With-RefreshToken.API/Program.cs
@@ -1,6 +1,3 @@
-using JWT_Authentication_With_RefreshToken.BL;
-using JWT_Authentication_With_RefreshToken.DAL;
-
 namespace JWT_Authentication_With_RefreshToken.API
 {
     public class Program
@@ -15,8 +12,6 @@ namespace JWT_Authentication_With_RefreshToken.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddSingleton<IJWTAuthenticationWithRefreshTokenBL,JWTAuthenticationWithRefreshTokenBL>();
-            builder.Services.AddSingleton<IJWTAuthenticationWithRefreshTokenDAL,JWTAuthenticationWithRefreshTokenDAL>();
 
             var app = builder.Build();
 
